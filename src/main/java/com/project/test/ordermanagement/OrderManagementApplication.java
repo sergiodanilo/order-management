0778e3ba9fe5c6cb8ed2daaa4fff2e5314f75e8d
Sérgio Holanda <sergio.danilo@gmail.com>
2025-04-06 19:31:23 -0300

    Update OrderManagementApplication.java

--- src/main/java/com/project/test/ordermanagement/OrderManagementApplication.java
+++ src/main/java/com/project/test/ordermanagement/OrderManagementApplication.java
@@ -3,11 +3,9 @@ package com.project.test.ordermanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
-@EnableScheduling
 public class OrderManagementApplication {
 
     public static void main(String[] args) {
